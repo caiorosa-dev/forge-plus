781bfaf2b769140156991e0f9517d436f9ab41b6
Caio Rosa <addminplayerbr1122@gmail.com>
2025-01-04 17:36:39 -0300

    fix: user folder

--- apps/desktop/src/main/helpers/get-mods-folder.ts
+++ apps/desktop/src/main/helpers/get-mods-folder.ts
@@ -1,9 +1,9 @@
 import path from 'path';
-import { getUser } from './get-user';
+import { app } from 'electron';
 
 export function getModsFolder(instanceName: string): string {
-	const user = getUser();
-	const instancesDir = `C:\\Users\\${user}\\curseforge\\minecraft\\Instances`;
+	const instancesDir = `${app.getPath('home')}\\curseforge\\minecraft\\Instances`;
+
 	const modsFolder = path.join(instancesDir, instanceName, 'mods');
 
 	return modsFolder;
--- apps/desktop/src/main/services/curse-forge/get-instances.ts
+++ apps/desktop/src/main/services/curse-forge/get-instances.ts
@@ -1,9 +1,8 @@
 import fs from 'fs';
-import { getUser } from '../../helpers/get-user';
+import { app } from 'electron';
 
 export function getCurseForgeInstances() {
-	const user = getUser();
-	const instancesDir = `C:\\Users\\${user}\\curseforge\\minecraft\\Instances`;
+	const instancesDir = `${app.getPath('home')}\\curseforge\\minecraft\\Instances`;
 
 	try {
 		const instances = fs.readdirSync(instancesDir, { withFileTypes: true })
