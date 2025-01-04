285a194fb2c666760fd3916bdf7fd8016f56f469
Caio Rosa <addminplayerbr1122@gmail.com>
2025-01-04 16:08:52 -0300

    fix: api key header name

--- apps/api/src/shared/api-key.guard.ts
+++ apps/api/src/shared/api-key.guard.ts
@@ -5,7 +5,7 @@ import { Request } from 'express';
 export class ApiKeyGuard implements CanActivate {
 	canActivate(context: ExecutionContext): boolean {
 		const request: Request = context.switchToHttp().getRequest();
-		const apiKey = request.headers['Authorization'];
+		const apiKey = request.headers['authorization'];
 
 		if (apiKey && apiKey === process.env.API_SECRET) {
 			return true;
